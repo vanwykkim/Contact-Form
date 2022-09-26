d96aeb6f8381e5a8bce70760b3fefb2893aac179
Kim Van Wyk <kmvanwyk@msn.com>
2022-09-26 02:59:09 -0700

    header is fixed at top

---
+++ assets/MyScript.js
@@ -0,0 +1,12 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+
+    window.alert("Thank you for trying my contact form!");
+
+}
+
