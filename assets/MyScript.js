57851989db8f80de07e9f0c0e390e8cb39404a5e
Kim Van Wyk <kmvanwyk@msn.com>
2022-09-26 04:43:07 -0700

    button class working

--- assets/MyScript.js
+++ assets/MyScript.js
@@ -1,10 +1,9 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
 
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+
+
+function myThankYou() {
 
     window.alert("Thank you for trying my contact form!");
 
