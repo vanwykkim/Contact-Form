bc2a1b7ded588dae5ce0a90fadf463f67ccf9510
Kim Van Wyk <kmvanwyk@msn.com>
2022-09-26 05:19:01 -0700

    working contact form

--- assets/MyScript.js
+++ assets/MyScript.js
@@ -1,11 +1,21 @@
 
-
-
+var fName = String;
+var lName = String;
 
 
 function myThankYou() {
 
     window.alert("Thank you for trying my contact form!");
 
+}
+
+function mySend(){
+    fName = document.getElementById("fname").value;
+    lName = document.getElementById("lname").value;
+    window.alert("You have saved the contact " + fName +" "+ lName);
+    document.getElementById("fname").value = '';
+    document.getElementById("lname").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("pnum").value = '';
 }
 
